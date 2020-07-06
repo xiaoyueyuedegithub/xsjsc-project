f7578b2404b992e4a5eb39affeaf3205bd5c3a37
LMQ_KX <690199224@qq.com>
2020-07-06 12:15:56 +0800

    Using Axios gets data from XSJ posterior system

--- src/main/java/edu/etime/xsjsc/utils/ResultUtil.java
+++ src/main/java/edu/etime/xsjsc/utils/ResultUtil.java
@@ -10,6 +10,39 @@ public class ResultUtil {
     private Integer code;
     private String msg;
     private Object data;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Object getInfo() {
+        return info;
+    }
+
+    public void setInfo(Object info) {
+        this.info = info;
+    }
+
     private Object info;
     /**
      * 返回一个info为空对象的成功消息的json
